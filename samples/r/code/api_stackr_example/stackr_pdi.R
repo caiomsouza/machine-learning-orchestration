47c197bbcf61c3935c26800d0d9bc6cffec80ebe
Caio Moreno de Souza <caiomsouza@gmail.com>
2017-03-30 17:53:29 +0200

    Add new samples ML using PDI and R

---
+++ samples/r/code/api_stackr_example/stackr_pdi.R
@@ -0,0 +1,12 @@
+
+library(stackr)
+
+stack_tags <- stack_tags(c("pentaho", "SPSS", "R", "Python", "weka", "Spark", "Hadoop", "BigData", "ibm", "oracle", "ctools", "kettle", "pdi", "saiku"))
+
+stack_tags
+
+
+
+
+
+
---
+++ samples/r/code/api_stackr_example/stackr_setup_install.R
@@ -0,0 +1,29 @@
+install.packages("jsonlite")
+
+install.packages("devtools")
+devtools::install_github("dgrtwo/stackr")
+
+# if you want to access the vignettes from within the package:
+devtools::install_github("dgrtwo/stackr", build_vignettes = TRUE)
+browseVignettes("stackr")
+
+library(stackr)
+
+q <- stack_questions()
+
+a <- stack_answers()
+
+stack_questions(11227809)
+
+stack_answers(c(179147, 2219560, 180085))
+
+stack_users(712603)
+
+stack_tags(c("r", "ggplot2", "dplyr"))
+
+stack_tags(c("ctools", "kettle", "pentaho", "weka", "oracle", "ibm"))
+
+stack_tags(c("saiku", "weka"))
+
+stack_tags(c("SPSS", "R", "Python", "weka", "Spark", "Hadoop", "BigData"))
+
