c6dceaf16d00bb5d8796d26b9f670ee7c6abd8bd
Caio Moreno <caiomsouza@gmail.com>
2017-07-24 18:08:21 +0200

    Added Python AutoML
    Added Python AutoML

---
+++ AutoML/src/python/automl_python/auto_ml_python.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 13 12:18:15 2017
+
+@author: cmoreno
+"""
+
+import autosklearn.classification
+import sklearn.model_selection
+import sklearn.datasets
+import sklearn.metrics
+X, y = sklearn.datasets.load_digits(return_X_y=True)
+X_train, X_test, y_train, y_test = \
+    sklearn.model_selection.train_test_split(X, y, random_state=1)
+automl = autosklearn.classification.AutoSklearnClassifier()
+automl.fit(X_train, y_train)
+y_hat = automl.predict(X_test)
+print("Accuracy score", sklearn.metrics.accuracy_score(y_test, y_hat))
